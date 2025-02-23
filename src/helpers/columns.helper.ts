1b94cc53ffa8f351e20897e2e2c9c0a501a93dfd
Chinh Chinh <128126308+phamdongchinh683@users.noreply.github.com>
2025-02-23 13:46:41 +0700

    Update columns.helper.ts

--- src/helpers/columns.helper.ts
+++ src/helpers/columns.helper.ts
@@ -2,7 +2,6 @@ import { sql } from 'drizzle-orm';
 import { timestamp } from 'drizzle-orm/pg-core';
 
 export const timestamps = {
-
   created_at: timestamp('created_at').default(sql`now()`),
   updated_at: timestamp('updated_at').default(sql`now()`),
 };
